2fb0cbc78849f99869e9d0f25523fca471326e4e
kishoripatil123 <110087746+kishoripatil123@users.noreply.github.com>
2022-07-28 13:41:03 +0530

    Add files via upload

---
+++ QuotientAndRemainder.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kdpat
+ */
+public class QuotientAndRemainder {
+  
+    public static void main(String[] args)
+    {
+  
+        int dividend = 556, divisor = 9;
+  
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+  
+        System.out.println("The Quotient is = " + quotient);
+        System.out.println("The Remainder is = " + remainder);
+    }
+}
