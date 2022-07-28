2ac479bef7f19eaadd90c720417baf4450e1a7aa
kishoripatil123 <110087746+kishoripatil123@users.noreply.github.com>
2022-07-28 13:37:33 +0530

    Add files via upload

---
+++ VowelConsonant.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kdpat
+ */
+public class VowelConsonant {
+
+    public static void main(String[] args) {
+
+        char ch = 'r';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+}
