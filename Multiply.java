01b121d4c752dde405aba0029dae600e917bac3e
kishoripatil123 <110087746+kishoripatil123@users.noreply.github.com>
2022-07-28 23:24:51 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kdpat
+ */
+public class Multiply {
+   public static void main(String[] args) {
+       int a;
+       int b;
+       int c;
+       a = 5;
+       b = 58;
+       c = a*b; //integer number to keep the result of multiplication
+       System.out.println("5*58 = " + c);
+   }
+}
