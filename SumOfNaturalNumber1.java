a783b39f455ef71e3901ebe440f9f3c10b5ae9fb
kishoripatil123 <110087746+kishoripatil123@users.noreply.github.com>
2022-07-28 13:47:22 +0530

    Add files via upload

---
+++ SumOfNaturalNumber1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kdpat
+ */
+public class SumOfNaturalNumber1  
+{  
+public static void main(String[] args)   
+{  
+int i, num = 10, sum = 0;  
+//executes until the condition returns true  
+for(i = 1; i <= num; ++i)  
+{  
+//adding the value of i into sum variable  
+sum = sum + i;  
+}  
+//prints the sum   
+System.out.println("Sum of First 10 Natural Numbers is = " + sum);  
+}  
+}  
