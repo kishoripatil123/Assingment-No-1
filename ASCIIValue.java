4d85f34ff8330c6539ff7a5b250d4343f56ca9c3
kishoripatil123 <110087746+kishoripatil123@users.noreply.github.com>
2022-07-28 23:22:38 +0530

    Add files via upload

---
+++ ASCIIValue.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kdpat
+ */
+public class ASCIIValue {
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+        // You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
